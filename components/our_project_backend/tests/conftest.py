de8c32f5518b1ebed7c2be6a09539edc273cac5a
Denis <den-kyder@mail.ru>
2022-03-19 23:23:34 +0300

    start made test

--- components/our_project_backend/tests/conftest.py
+++ components/our_project_backend/tests/conftest.py
@@ -1,3 +1,98 @@
 import pytest
 
-# from simple_chat.application import dataclasses
+from simple_chat.application import dataclasses
+
+
+@pytest.fixture(scope='function')
+def user_1():
+    return dataclasses.User(
+        id=1,
+        login='test1',
+        password='password'
+    )
+
+
+@pytest.fixture(scope='function')
+def user_2():
+    return dataclasses.User(
+        id=2,
+        login='test2',
+        password='password'
+    )
+
+
+@pytest.fixture(scope='function')
+def user_3():
+    return dataclasses.User(
+        id=3,
+        login='test3',
+        password='password'
+    )
+
+
+@pytest.fixture(scope='function')
+def chat_message_1():
+    return dataclasses.ChatMessage(
+        id=1,
+        user_id=1,
+        chat_id=1,
+        text='text'
+    )
+
+
+@pytest.fixture(scope='function')
+def chat_message_2():
+    return dataclasses.ChatMessage(
+        id=2,
+        user_id=1,
+        chat_id=1,
+        text='text2'
+    )
+
+
+@pytest.fixture(scope='function')
+def chat_member_1():
+    return dataclasses.ChatMember(
+        id=1,
+        user_id=1,
+        chat_id=1,
+    )
+
+
+@pytest.fixture(scope='function')
+def chat_member_2():
+    return dataclasses.ChatMember(
+        id=1,
+        user_id=2,
+        chat_id=1,
+    )
+
+
+@pytest.fixture(scope='function')
+def chat_member_3():
+    return dataclasses.ChatMember(
+        id=1,
+        user_id=2,
+        chat_id=2,
+    )
+
+
+@pytest.fixture(scope='function')
+def chat_1():
+    return dataclasses.Chat(
+        id=1,
+        user_id=1,
+        title='TestTitle_owner_1',
+        members=[chat_member_1(), chat_member_2()],
+        messages=[chat_message_1(), chat_message_2()]
+    )
+
+
+@pytest.fixture(scope='function')
+def chat_2():
+    return dataclasses.Chat(
+        id=2,
+        user_id=2,
+        title='TestTitle_owner_2',
+        members=[chat_member_2()]
+    )
--- components/our_project_backend/tests/unit/application/conftest.py
+++ components/our_project_backend/tests/unit/application/conftest.py
@@ -2,4 +2,36 @@ from unittest.mock import Mock
 
 import pytest
 
-# from simple_chat.application import interfaces
+from simple_chat.application import interfaces
+
+
+@pytest.fixture(scope='function')
+def users_repo(user_1, user_2, user_3):
+    users_repo = Mock(interfaces.UsersRepo)
+    users_repo.get_by_user_id = Mock(return_value=[user_1, user_2, user_3])
+    return users_repo
+
+
+@pytest.fixture(scope='function')
+def chats_repo(chat_1, chat_2):
+    chats_repo = Mock(interfaces.ChatsRepo)
+    chats_repo.get_by_chat_id = Mock(return_value=[chat_1, chat_2])
+    return chats_repo
+
+
+@pytest.fixture(scope='function')
+def messages_repo(chat_message_1, chat_message_2):
+    messages_repo = Mock(interfaces.ChatMessagesRepo)
+    messages_repo.get_by_message_id = Mock(
+        return_value=[chat_message_1, chat_message_2]
+    )
+    return messages_repo
+
+
+@pytest.fixture(scope='function')
+def members_repo(chat_member_1, chat_member_2, chat_member_3):
+    members_repo = Mock(interfaces.ChatMembersRepo)
+    members_repo.get_by_member_id = Mock(
+        return_value=[chat_member_1, chat_member_2, chat_member_3]
+    )
+    return members_repo
